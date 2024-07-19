25c1e8959e937e3cc0ff16d7f0741e54698cca9f
bhunao <bhunao@gmail.com>
2024-07-18 22:51:21 -0300

    A HAPPY FLOWER

---
+++ zig/free/a_plant.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    print("       \\_|_/         \n", .{});
+    print("       =|_|=         \n", .{});
+    print("       //||\\         \n", .{});
+    print("   \\     ||      /   \n", .{});
+    print("    \\==========/     \n", .{});
+    print("         ||          \n", .{});
+    print("         ||          \n", .{});
+    print("         ||          \n", .{});
+    print("         ||          \n", .{});
+    print("_________||__________\n", .{});
+}
